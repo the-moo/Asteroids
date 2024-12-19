611aa9142b33b2ec1bddd0a9e70cc3f6b4732179
The Moo <104044225+the-moo@users.noreply.github.com>
2024-12-19 16:19:33 +0000

    player.py and circles added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -11,12 +12,22 @@ def main():
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    clock = pygame.time.Clock()
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT /2
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
+        dt = clock.tick(60) / 1000 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
 
 if __name__ == "__main__":
     main()
